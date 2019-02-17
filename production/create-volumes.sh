35e9d836a992301b863fbcc5f626b6139311522f
Drew Moseley <drew.moseley@northern.tech>
2019-02-17 14:46:08 -0500

    Revert "Setup for S3"
    This reverts commit 379a423efe6a63443f5ad8f53972abbf2120ac92.

--- production/create-volumes.sh
+++ production/create-volumes.sh
@@ -1,3 +1,4 @@
+docker volume create --name=mender-artifacts-drew
 docker volume create --name=mender-db
 docker volume create --name=mender-elasticsearch-db
 docker volume create --name=mender-redis-db
